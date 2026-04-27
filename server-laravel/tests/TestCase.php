6396236e118dd7dc798db16c1da55efc01aedc00
CBElmer28 <cbelmer28@gmail.com>
2026-04-27 18:11:54 -0500

    .

--- server-laravel/tests/TestCase.php
+++ server-laravel/tests/TestCase.php
@@ -8,16 +8,5 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        // Con SQLite en memoria las tablas no existen todavía.
-        // Corremos las migraciones automáticamente para que DatabaseTransactions
-        // pueda envolver los tests correctamente sin afectar datos reales.
-        if (config('database.default') === 'sqlite') {
-            $this->artisan('migrate');
-        }
-    }
 }
 
